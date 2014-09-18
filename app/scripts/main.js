665566e3d335a58eceda5a3c2399b8889e2f5d02
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2014-09-18 07:57:49 +0200

    processing of sample data into node/edge lists

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,6 @@
 (function() {
   'use strict';
-  var sampleRelations = [{ //{{{1
+  var sampleItem = [{ //{{{1
     property: 'Cover',
     value: 'http://dev.vejlebib.dk/sites/default/files/styles/ding_primary_large/public/ting/covers/object/796e550251e19f9e2deeb270d0d80670.jpg?itok=jAqN8JPD'
   }, {
@@ -10,6 +10,9 @@
     property: 'Titel',
     value: 'Siddhartha - en indisk legende'
   }, {
+    property: 'Collection',
+    value: 'ting:870970-basis%3A22331892'
+  }, {
     property: 'Creator',
     value: 'Hermann Hesse (2012)'
   }, {
@@ -18,9 +21,6 @@
   }, {
     property: 'Serie',
     value: 'Gyldendals paperbacks'
-  }, {
-    property: 'Emne',
-    value: 'religiøse bøger'
   }, {
     property: 'Id',
     value: 'ting:870970-basis%3A23243431'
@@ -132,9 +132,80 @@
     value: 'ting:870970-basis%3A15153431',
     title: 'Skønt Siddhartha, med skiftende tider har mistet sin kultstatus, er temaet: jeg\'ets søgen efter meningen med tilværelsen, dog evigt aktuelt, og Hesses kendte roman skal selvfølgelig også fremover være at finde på biblioteket. Den egner sig godt til læsning i studiekredse'
   }];
+  //{{{1 d3 force layout experiment
+  var categories = {
+    authorInfo: ['Om forfatteren', 'Creator'],
+    review: ['Anmeldelse', 'Lektørudtalelse'],
+    structure: ['Serie', 'Udgave', 'Collection'],
+    circular: ['Publikum', 'Emne', 'Sprog', 'Serie']
+  };
+
+  function createNodesExternal(item) { //{{{2
+    var root, nodes, edges, i, rel, categoryMap, categoryNodes, category, property, node;
+
+    // {{{3 graph definition and root nodes
+    root = {
+      type: 'root',
+      visible: true
+    };
+    nodes = [root];
+    edges = [];
+
+
+    // {{{3 create nodes for categories to get clouds
+    categoryNodes = {};
+    categoryMap = {};
+    for (category in categories) {
+      if (categories.hasOwnProperty(category)) {
+        categoryNodes[category] = {
+          label: category,
+          type: 'category',
+          visible: false
+        };
+        nodes.push(categoryNodes[category]);
+        for (i = 0; i < categories[category].length; ++i) {
+          categoryMap[categories[category][i]] = category;
+        }
+      }
+    }
+
+    // {{{3 nodes for individual ralations
+    for (i = 0; i < item.length; ++i) {
+      rel = item[i];
+      property = rel.property;
+      category = categoryMap[property];
+      console.log(property, category, categoryMap);
+      if (category) {
+        node = {
+          label: property + '\n' + (rel.title || rel.value),
+          property: property,
+          value: rel.value,
+          visible: true
+        };
+        nodes.push(node);
+        edges.push({
+          source: categoryNodes[category],
+          target: node
+        });
+        edges.push({
+          source: root,
+          target: node
+        });
+      }
+    }
+    return {
+      nodes: nodes,
+      edges: edges
+    };
+  }
+  //{{{2
+  console.log(createNodesExternal(sampleItem));
+
+
+
   //{{{1 code for testing/demo
   $(function() {
-    document.body.innerHTML += JSON.stringify(sampleRelations);
+    document.body.innerHTML += JSON.stringify(sampleItem);
 
     // button on sample page pops up visualisation
     $('#relvis-button').click(function() {
