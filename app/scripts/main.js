30305b59f59c4e3ad4fda420ba397933dfc41ff7
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2014-09-24 21:20:29 +0200

    fit canvas text to box

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,11 +2,53 @@
   'use strict';
   var visualObjectRatio = 13 / 8;
   var margin = 0.05;
-  //util {{{1
-  function square(a) {
+  //{{{1 canvas text drawing
+  function textLayout(ctx, str, width) { //{{{2
+    window.ctx = ctx;
+    var spacing = ctx.measureText(' ').width;
+    var words = str.split(" ");
+    var lengths = words.map(function(word) {
+      return ctx.measureText(word).width;
+    });
+    var lines = [];
+    var line = [];
+    var linePos = 0;
+    for(var i = 0; i < words.length; ++i) {
+      if(linePos > 0 && linePos + lengths[i] > width)  {
+        lines.push({str: line.join(" "), len: linePos});
+        line = [];
+        linePos = 0;
+      }
+      line.push(words[i]);
+      linePos += lengths[i] + spacing;
+    }
+    lines.push({str: line.join(" "), len: linePos});
+    return lines;
+  }
+  function writeBox(ctx, str, x, y, w, h) { //{{{2
+    var size = 60;
+    var lines, i, maxLen;
+    do {
+      size = size * 0.9 |0;
+      ctx.font = size + "px sans-serif";
+      lines = textLayout(ctx, str, w);
+      maxLen = 0;
+      for(i=0;i<lines.length;++i) {
+        maxLen = Math.max(maxLen, lines[i].len);
+      }
+    } while(size > 13 && (maxLen> w || lines.length * size > h));
+
+    for(i = 0; i < lines.length; ++i) {
+      if(size * (i+1) < h) {
+        ctx.fillText(lines[i].str, x, y + size * (i + 1));
+      }
+    }
+  }
+  window.textLayout = textLayout;
+  // Utility functions {{{1
+  function square(a) { //{{{2
     return a * a;
   }
-
   function nearestPoints(list) { //{{{2
     function assignDist(a, b, dist) {
       if (a.nearestDist > dist) {
@@ -338,7 +380,9 @@
         //, sz.x*canvas.width, sz.y*canvas.height);
         ctx.font = '20px sans serif';
         ctx.fillStyle = '#f00';
-        ctx.fillText(node.label.slice(0, 40), node.x - size * 0.9, node.y + 5);
+        writeBox(ctx, node.label,
+            node.x - size, node.y - size/visualObjectRatio,
+            size * 2, size * 2 / visualObjectRatio);
       }
     });
   }
