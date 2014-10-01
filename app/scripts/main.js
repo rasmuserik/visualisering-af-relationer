46a897fdb024d32270f051ea28b06d23525e5955
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2014-10-01 19:09:13 +0200

    indentation

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,18 +1,18 @@
-  $(function() {
-    'use strict';
+$(function() {
+  'use strict';
 
-    function showGraph() {
-      var relvis = window.relvis;
-      relvis.createGraph();
-      relvis.layoutGraph();
-      relvis.showCanvasOverlay();
-    }
+  function showGraph() {
+    var relvis = window.relvis;
+    relvis.createGraph();
+    relvis.layoutGraph();
+    relvis.showCanvasOverlay();
+  }
 
-    // button on sample page pops up visualisation
-    $('#relvis-button').click(showGraph);
+  // button on sample page pops up visualisation
+  $('#relvis-button').click(showGraph);
 
-    // show visualisation on load if we have #relvis hash
-    if (location.hash === '#relvis') {
-      showGraph();
-    }
-  });
+  // show visualisation on load if we have #relvis hash
+  if (location.hash === '#relvis') {
+    showGraph();
+  }
+});
