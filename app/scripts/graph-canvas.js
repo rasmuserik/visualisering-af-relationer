0128310191485ddfbe3d16a6bc5380efdd8c30a9
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2014-10-08 14:46:49 +0200

    coordinate transformation

--- app/scripts/graph-canvas.js
+++ app/scripts/graph-canvas.js
@@ -2,6 +2,25 @@
   'use strict';
   var relvis = window.relvis = window.relvis || {};
 
+  // vector math, - maybe move this into util
+  var xy = {};
+
+  xy.mul = function(p1, p2) { return { x: p1.x * p2.x, y: p1.y * p2.y, }; };
+  xy.add = function(p1, p2) { return { x: p1.x + p2.x, y: p1.y + p2.y, }; };
+  xy.sub = function(p1, p2) { return { x: p1.x - p2.x, y: p1.y - p2.y, }; };
+  xy.scale = function(p, a) { return { x: p.x * a, y: p.y * a, }; };
+  xy.inv = function(p) { return {x: 1/p.x, y: 1/p.y}; };
+
+  // transformations to/from canvas coordinates
+  relvis.offset = {x: 123, y:456}; // odd start offset for testing
+  relvis.scale = {x:7, y:8}; // odd start offset for testing
+  relvis.toCanvasCoord = function(p) {
+    return xy.mul(xy.sub(p, relvis.offset), relvis.scale);
+  };
+  relvis.toGraphCoord = function(p) {
+    return xy.add(xy.mul(p, xy.inv(relvis.scale)), relvis.offset);
+  };
+
   function drawGraph() { //{{{1
     if (!relvis.nodes || !relvis.overlayVisible) {
       return;
@@ -18,13 +37,21 @@
     ctx.fillStyle = 'rgba(0,0,0,0.2)';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    // Calculate view coordinates for all points
+    // Find coordinate transformation
     var margin = 0.05;
     var boundaries = relvis.findBoundaries(visibleNodes, ['x', 'y']);
+
+    relvis.offset = xy.sub(boundaries.min, 
+        xy.scale(boundaries.range, margin));
+    relvis.scale = xy.scale(boundaries.range, 1 + 2* margin);
+    relvis.scale = xy.mul({x: canvas.width, y: canvas.height},
+        xy.inv(relvis.scale));
+
+    // Calculate view coordinates for all points
     visibleNodes.forEach(function(node) {
-      var xy = boundaries.zeroOne(node);
-      node.vx = (xy.x * (1 - 2 * margin) + margin) * canvas.width;
-      node.vy = (xy.y * (1 - 2 * margin) + margin) * canvas.height;
+      var p = relvis.toCanvasCoord(node);
+      node.vx = p.x;
+      node.vy = p.y;
     });
     relvis.nearestPoints(visibleNodes, 'vx', 'vy');
 
--- test/spec/test.js
+++ test/spec/test.js
@@ -44,6 +44,8 @@
   });
   describe('graph-canvas', function() {
     it('should run here few assertions', function() {
+      assert(relvis.toGraphCoord(relvis.toCanvasCoord({x: 5, y: 7})).x === 5);
+      assert(relvis.toGraphCoord(relvis.toCanvasCoord({x: 5, y: 7})).y === 7);
       // drawGraph - not easily testable
       // requestRedraw not easily testable
     });
