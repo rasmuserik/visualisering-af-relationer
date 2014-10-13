974a854a4d376db245774d3383a36b09471ded3a
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2014-10-13 17:18:10 +0200

    beautification

--- app/scripts/graph-model.js
+++ app/scripts/graph-model.js
@@ -15,76 +15,77 @@
     var root, nodes, edges, i, rel, categoryMap, categoryNodes, category, property, node, categoryNodeList;
 
     function createCategoryNodes() { // {{{2
-    categoryNodes = {};
-    categoryMap = {};
-    categoryNodeList = [];
-    for (category in categories) {
-      if (categories.hasOwnProperty(category)) {
-        categoryNodes[category] = {
-          label: category,
-          type: 'category',
-          visible: false
-        };
-        categoryNodeList.push(categoryNodes[category]);
-        nodes.push(categoryNodes[category]);
-        for (i = 0; i < categories[category].length; ++i) {
-          categoryMap[categories[category][i]] = category;
+      categoryNodes = {};
+      categoryMap = {};
+      categoryNodeList = [];
+      for (category in categories) {
+        if (categories.hasOwnProperty(category)) {
+          categoryNodes[category] = {
+            label: category,
+            type: 'category',
+            visible: false
+          };
+          categoryNodeList.push(categoryNodes[category]);
+          nodes.push(categoryNodes[category]);
+          for (i = 0; i < categories[category].length; ++i) {
+            categoryMap[categories[category][i]] = category;
+          }
         }
       }
-    }
-    categoryNodeList[0].fixedPosition = {
-      x: 0,
-      y: 0
-    };
-    categoryNodeList[1].fixedPosition = {
-      x: 1,
-      y: 0
-    };
-    categoryNodeList[2].fixedPosition = {
-      x: 0,
-      y: 1
-    };
-    categoryNodeList[3].fixedPosition = {
-      x: 1,
-      y: 1
-    };
+      categoryNodeList[0].fixedPosition = {
+        x: 0,
+        y: 0
+      };
+      categoryNodeList[1].fixedPosition = {
+        x: 1,
+        y: 0
+      };
+      categoryNodeList[2].fixedPosition = {
+        x: 0,
+        y: 1
+      };
+      categoryNodeList[3].fixedPosition = {
+        x: 1,
+        y: 1
+      };
     }
 
     function createRelationNodes() { //{{{2
-    Object.keys(categories).forEach(function(category) {
-      categories[category].forEach(function(property) {
-        relvis.getValues(id, property).forEach(function(value) {
-          node = {
-            label: value,
-            property: property,
-            value: value,
-            visible: true
-          };
-          if (node.label.slice(0, 5) === 'ting:') {
-            node.label = relvis.getValues(node.label, 'Titel')[0] || '...';
-          }
-          nodes.push(node);
-          edges.push({
-            source: categoryNodes[category],
-            target: node
-          });
-          edges.push({
-            source: root,
-            target: node
+      Object.keys(categories).forEach(function(category) {
+        categories[category].forEach(function(property) {
+          relvis.getValues(id, property).forEach(function(value) {
+            node = {
+              label: value,
+              property: property,
+              value: value,
+              visible: true
+            };
+            if (node.label.slice(0, 5) === 'ting:') {
+              node.label = relvis.getValues(node.label, 'Titel')[0] || '...';
+            }
+            nodes.push(node);
+            edges.push({
+              source: categoryNodes[category],
+              target: node
+            });
+            edges.push({
+              source: root,
+              target: node
+            });
           });
         });
       });
-    });
     }
+
     function createRootNode() { //{{{2
-    root = {
-      label: 'root',
-      type: 'root',
-      visible: true
-    };
-    root.imgSrc = relvis.getValues(id, 'Cover')[0];
-    root.label = relvis.getValues(id, 'Titel')[0];
-    nodes.push(root);
+      root = {
+        label: 'root',
+        type: 'root',
+        visible: true
+      };
+      root.imgSrc = relvis.getValues(id, 'Cover')[0];
+      root.label = relvis.getValues(id, 'Titel')[0];
+      nodes.push(root);
     }
 
     nodes = relvis.nodes = []; //{{{2
