921fb4f239aa17f54842a78f836561654e93c2dc
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2014-10-14 17:54:53 +0200

    restructure ui.js

--- app/scripts/ui.js
+++ app/scripts/ui.js
@@ -2,43 +2,50 @@
   'use strict';
   var relvis = window.relvis = window.relvis || {};
 
-  relvis.initUI = function() { //{{{1
-    var info = '';
-    relvis.addEventListener('redraw', function() {
-      var ctx = relvis.canvas.getContext('2d');
-      ctx.fillStyle = '#fff';
-      ctx.fillRect(0, 0, 500, 10);
-      ctx.font = '10px sans-serif';
-      ctx.fillStyle = '#000';
-      ctx.fillText(info + ' ' + Date.now(), 10, 10);
-    });
+  var x0, y0;
 
-    function showStatus(text) {
-      info = text;
-    }
-    relvis.addEventListener('tapstart', function(e) {
-      var node = (e.node || {});
-      showStatus('tapstart ' + JSON.stringify({
-        x: e.x,
-        y: e.y,
-        node: node.label
-      }));
-      node.fixed = true;
-      relvis.fixedViewport = true;
-      relvis.requestRedraw();
-    });
-    relvis.addEventListener('tapmove', function(e) {
-      showStatus('tapmove' + JSON.stringify({
-        x: e.x,
-        y: e.y,
-        node: (e.node || {}).label
-      }));
-      relvis.requestRedraw();
-    });
-    relvis.addEventListener('tapend', function(e) {
-      showStatus('tapend');
-      relvis.fixedViewport = false;
-      relvis.requestRedraw();
-    });
+  var info = '';
+
+
+  function redraw() { //{{{1
+    var ctx = relvis.canvas.getContext('2d');
+    ctx.fillStyle = '#fff';
+    ctx.fillRect(0, 0, 500, 10);
+    ctx.font = '10px sans-serif';
+    ctx.fillStyle = '#000';
+    ctx.fillText(info + ' ' + Date.now(), 10, 10);
+  }
+
+  function showStatus(text) { //{{{1
+    info = text;
+  }
+
+  relvis.addEventListener('tapstart', function(e) {
+    var node = (e.node || {});
+    showStatus('tapstart ' + JSON.stringify({
+      x: e.x,
+      y: e.y,
+      node: node.label
+    }));
+    node.fixed = true;
+    relvis.fixedViewport = true;
+    relvis.requestRedraw();
+  });
+  relvis.addEventListener('tapmove', function(e) {
+    showStatus('tapmove' + JSON.stringify({
+      x: e.x,
+      y: e.y,
+      node: (e.node || {}).label
+    }));
+    relvis.requestRedraw();
+  });
+  relvis.addEventListener('tapend', function(e) {
+    showStatus('tapend');
+    relvis.fixedViewport = false;
+    relvis.requestRedraw();
+    relvis.layoutGraph();
+  });
+  relvis.initUI = function() { //{{{1
+    relvis.addEventListener('redraw', redraw);
   };
 })(); //{{{1
