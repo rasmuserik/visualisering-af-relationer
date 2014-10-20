61fafa080bdf1c31150076a90b39642e370b537c
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2014-10-20 17:03:56 +0200

    work on adhl-processing

--- adhl/generate_database.js
+++ adhl/generate_database.js
@@ -11,23 +11,30 @@
 
   var coloansDB = levelup('coloans.leveldb');
   var lidInfoDB = levelup('lid-info.leveldb');
+  var lidDB = levelup('lid.leveldb');
+  var patronDB = levelup('patron.leveldb');
 
   var t0 = Date.now();
-  var statStep = 25000;
+  var statStep =  2500;
   var prevPatron;
   var lendlist = [];
   var patrons = [];
   var lids = {};
   var nextId = 0;
   var coloanCount = 0;
+  var minColoans = 3;
+  var maxSize = 10000;
+  var sampleSize = 1000; // number of patrons to sample
+  var lidStat = {};
 
   var limit;
   limit = 47196844;
-  limit = 100000;
+  limit = 3000;
 
   function stat() {}
 
   pass1();
+  var lidCount = 0;
 
   function pass1() {
     lineReader.eachLine('../../final_adhl.sorted.csv', function(line, last, cb) {
@@ -43,6 +50,29 @@
       var author = fields[9];
       var kind = fields[10];
 
+      update(lidDB, lid, patron, function() {
+        update(patronDB, patron, lid, function() {
+          next();
+        });
+      });
+
+      function update(db, key, val, cb) {
+        db.get(key, function(err, data) {
+          if(err) {
+            var elems = [];
+          } else {
+            elems = data.split(',');
+          }
+          if(elems.length < maxSize && !(val in elems)) {
+            elems.push(val);
+            db.put(key, String(elems), cb); 
+          } else {
+            cb();
+          }
+        });
+      }
+
+      lidStat[lid] = (lidStat[lid] || 0) + 1;
       lidInfoDB.get(lid, function(err, obj) {
         if (err) {
           obj = {
@@ -51,6 +81,7 @@
             kind: kind,
             count: 1
           };
+          ++lidCount;
         } else {
           obj = JSON.parse(obj);
           ++obj.count;
@@ -62,30 +93,75 @@
         })
       });
 
-      if (patron === prevPatron) {
-        if (!(lid in lendlist)) {
-          lendlist.push(lid);
-        }
-      } else {
-        lendlist.sort();
-        if (lendlist.length < 3000 && lendlist.length > 1) {
-          for (var i = 0; i < lendlist.length; ++i) {}
-          coloansDB.put(patron, String(lendlist));
-          ++coloanCount;
-        }
-        prevPatron = patron;
-        lendlist = [];
-      }
 
-      if (count > limit || last) {
-        pass2();
-      } else {
-        cb();
+      function next() {
+        if (count > limit || last) {
+          pass2();
+        } else {
+          cb();
+        }
       }
     });
   }
 
-  function pass2() {}
+  function pass2() {
+    console.log('pass2', lidCount);
+    lidInfoDB.createReadStream()
+      .on('data', function(data) {
+        var lid = data.key;
+        lidDB.get(lid, function(err, data) {
+          if(err) {
+            console.log(err);
+          }
+          var patrons = data.split(',').slice(0, sampleSize);
+          var sampleCount = patrons.length;
+          var borrows = {};
+          handlePatrons();
+          function handlePatrons() {
+            if(!patrons.length) {
+              return chooseColoans();
+            }
+            var patron = patrons.pop();
+            patronDB.get(patron, function(err, data) {
+              if(err) {
+                console.log(err);
+              }
+              var lids = data.split(',');
+              lids.forEach(function(lid) {
+                if(!borrows[lid]) {
+                  borrows[lid] = 1;
+                } else {
+                  ++borrows[lid];
+                }
+              });
+              handlePatrons();
+            });
+          }
+          function chooseColoans() {
+            var coloans = [];
+            Object.keys(borrows).forEach(function(lid) {
+              coloans.push({
+                lid: lid,
+                coloans: borrows[lid],
+                total: lidStat[lid]
+              });
+            });
+            coloans.sort(function(a,b) {
+              return a.coloans < b.coloans;
+            });
+            if(sampleCount > 1) {
+              console.log(lid, JSON.stringify(coloans.slice(0, 30)));
+            }
+          }
+        });
+    }).on('end', function() {
+      setTimeout(pass3, 3000);
+    });
+
+  }
+  function pass3() {
+    console.log('pass3');
+  }
 
 
   /*
--- adhl/generate_database.py
+++ adhl/generate_database.py
@@ -1,14 +1,19 @@
 import leveldb
 
-db = leveldb.LevelDB("adhl.leveldb", error_if_exists=True)
+lidDB = leveldb.LevelDB("lids.leveldb", error_if_exists=True)
+patronDB = leveldb.LevelDB("patrons.leveldb", error_if_exists=True)
+lidInfoDB = leveldb.LevelDB("lid-info.leveldb", error_if_exists=True)
 
-count = 0;
+count = 0
+limit = 100000
 
 for line in file("../../final_adhl.csv"):
 
     count = count + 1
-    if count % 100000 == 0:
+    if count % 10000 == 0:
         print count
+    if count > limit:
+        break
 
     fields = line.split(",")
 
@@ -18,3 +23,22 @@ for line in file("../../final_adhl.csv"):
         title = fields[8]
         author = fields[9]
         kind = fields[10]
+
+        lidDB.Put(lid + ':' + patron, 't');
+        patronDB.Put(patron + ':' + lid, 't');
+
+stat = {}
+for key, val in lidDB.RangeIter("0", ":"):
+    lid = key.split(':')[0]
+    stat[lid] = stat.get(lid, 0) + 1
+
+for lid, count in stat:
+    coloans = {}
+    for key, _ in lidDB.RangeIter(lid,lid+';'):
+        patron = key.split(':')[1]
+        for key, _ in lidDB.RangeIter(patron,patron+';'):
+            coloan = key.split(':')[1]
+            coloans[coloan] = coloans.get(coloan, 0) + 1
+
+
+    print coloans
